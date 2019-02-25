86133b10b8240afb6a9bd258422096a91df01c92
Kochetkov Nickolai <vlegros@21-SCHOOL.RU>
2019-02-25 23:32:20 +0300

    adding final modifications

--- setup_server.sh
+++ setup_server.sh
@@ -1,9 +1,14 @@
+RED='\033[0;31m'         #  ${RED}
+GREEN='\033[0;32m'      #  ${GREEN}
+GRAY='\033[0;37m'       #  ${GRAY}  
+CYAN='\033[0;36m'       #  ${CYAN}
+
 #CREATING SUDO USER
 if [ -n "$1" ]
 then
 	if adduser hey 2>&1 | grep "Permission denied" > /dev/null
 	then
-		echo "YOU SHOULD BE ROOT OR SUDO TO RUN THIS SCRIPT"
+		echo "${RED}YOU SHOULD BE ROOT OR SUDO TO RUN THIS SCRIPT${GRAY}"
 		exit 0
 	fi
 	echo  "ADDING NEW USER..."
@@ -11,9 +16,9 @@ then
 	passwd $1 2> /dev/null
 	echo  "MAKING HIM SUDO..."
 	usermod -aG wheel $1
-	echo 	"DONE..."
+	echo 	" ${GREEN}DONE...${GRAY}"
 else
-	echo "TYPE THE USER NAME YOU WOULD LIKE TO CREARTE AND ADD TO SUDO GROUP"
+	echo "${RED}TYPE THE USER NAME YOU WOULD LIKE TO CREARTE AND ADD TO SUDO GROUP${GRAY}"
 	exit 0
 fi
 
@@ -24,12 +29,14 @@ cat ifcfg-enp0s3 > /etc/sysconfig/network-scripts/ifcfg-enp0s3
 echo  "REBOOTING NETWORK ADAPTER..."
 systemctl restart network 
 echo "NEW IP IS 192.168.20.7/30" > server_config
+echo "${GREEN}DONE...${GRAY}"
 
 
 #SSH CONFIG 
 echo "CHANGING SSH PORT..."
 sshd_config > /etc/ssh/sshd_config
 echo "NEW SSH PORT IS 50683" >> server_config
+echo "${GREEN}DONE...${GRAY}"
 
 
 #SETTING FIREWALL
@@ -41,19 +48,19 @@ systemctl disable firewalld.service > /dev/null
 sh iptables_config.sh
 systemctl enable iptables.service > /dev/null
 systemctl restart iptables.service > /dev/null
-echo "DONE.."
+echo "${GREEN}DONE..${GRAY}"
 echo "FAIL2BAN..."
 yum install epel-release -y > /dev/null
 yum install fail2ban -y > /dev/null
 systemctl enable fail2ban > /dev/null
-echo "DONE..."
+echo "${GREEN}DONE...${GRAY}"
 
 
 #SETTING CRONTAB
 echo "SETTING CRONTAB..."
 crontab -r
 cat root | crontab - > /dev/null
-echo "DONE..."
+echo "${GREEN}DONE...${GRAY}"
 
 
 #INSTALLING NGINX
@@ -61,28 +68,35 @@ echo "INSTALLING NGINX..."
 yum -y install nginx > /dev/null
 systemctl start nginx > /dev/null
 systemctl enable nginx > /dev/null
-echo "DONE..."
-#echo "SETTING SSL..."
-#mkdir /etc/nginx/ssl
-#chmod 700 /etc/nginx/ssl
-#mkdir /etc/nginx/ssl/private
-#chmod 700 /etc/nginx/ssl/private
-#mkdir /etc/nginx/ssl/certs
-#chmod 700 /etc/nginx/ssl/certs
-#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/private/nginx-selfsigned.key -out /etc/nginx/ssl/certs/nginx-selfsigned.crt
-#openssl dhparam -out /etc/nginx/ssl/certs/dhparam.pem 2048 > /dev/null
-#cat nginx.conf > /etc/nginx/nginx.conf
-#cat ssl.conf > /etc/nginx/conf.d/ssl.conf
+echo "${GREEN}DONE...${GRAY}"
+echo "SETTING SSL..."
+mkdir /etc/nginx/ssl
+chmod 700 /etc/nginx/ssl
+mkdir /etc/nginx/ssl/private
+chmod 700 /etc/nginx/ssl/private
+mkdir /etc/nginx/ssl/certs
+chmod 700 /etc/nginx/ssl/certs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/private/nginx-selfsigned.key -out /etc/nginx/ssl/certs/nginx-selfsigned.crt
+echo "DHPARAM KEY..."
+openssl dhparam -out /etc/nginx/ssl/certs/dhparam.pem 2048 2> /dev/null
+cat nginx.conf > /etc/nginx/nginx.conf
+cat ssl.conf > /etc/nginx/conf.d/ssl.conf
 systemctl restart nginx
-echo "DONE..."
+echo "${GREEN}DONE...${GRAY}"
 
 #FILE MANEGEMENT
-#mkdir /root/sys_scripts
-#chmod 700 sys_scripts
-#mv update_system.sh /root/sys_scripts/
-#mv check_cron_changes.sh /root/sys_scripts/
-#mv iptables_config.sh /root/sys_scripts/
-#mv change_hashsum.sh /root/sys_scripts/
-#mv server_config /root
-#rm root
-echo "SERVER CONFIGURATION COMPLETED"
+mkdir /root/sys_scripts
+mkdir /root/config_files
+chmod 700 /root/config_files
+chmod 700 /root/sys_scripts
+mv ifcfg-enp0s3 /root/config_files/
+mv nginx.conf /root/config_files/
+mv sshd_config /root/config_files/
+mv ssl.conf /root/config_files/
+mv update_system.sh /root/sys_scripts/
+mv check_cron_changes.sh /root/sys_scripts/
+mv iptables_config.sh /root/sys_scripts/
+mv change_hashsum.sh /root/sys_scripts/
+mv server_config /root
+rm root
+echo "${CYAN}SERVER CONFIGURATION COMPLETED${GRAY}"
